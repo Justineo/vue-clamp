3cd0cf85085768b0c4c61dc3aaf26ed13961b9e7
Justineo <justice360@gmail.com>
2026-05-21 12:35:58 +0800

    chore: support Vue SFC package builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
+import vue from "@vitejs/plugin-vue";
+import Vue from "unplugin-vue/rolldown";
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  plugins: [vue()],
+  pack: {
+    dts: { vue: true },
+    platform: "neutral",
+    plugins: [Vue({ isProduction: true })],
+  },
   staged: {
     "*": "vp check --fix",
   },
